1fb3a472d7fce1823b14d7e07475648a2c0269da
Andrey Stoyanov <chessterfield@gmail.com>
2026-09-17 01:20:21 +0300

    Implemented room offers functionality

--- solution.js
+++ solution.js
@@ -1,5 +1,4 @@
-let reservation =
-{
+const reservation = {
     startDate: null,
     endDate: null,
     guestsCount: 0,
@@ -7,17 +6,78 @@ let reservation =
     name: null,
     phone: null,
     email: null
-}
+};
 
 function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
+    document
+        .querySelectorAll('.custom-form')
+        .forEach(div => div.classList.add('hidden'));
+
+    const content = document.querySelector(`.${className}`);
+
+    if (content !== null) {
+        content.classList.remove('hidden');
     }
 }
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+changeContent('search-result-form-content');
 
-function cleanData(e) {
+document
+    .querySelector('#search-back-btn')
+    .addEventListener('click', fillSearchForm);
+
+document
+    .querySelectorAll('.room-type')
+    .forEach(room => room.addEventListener('click', selectRoomType));
+
+document
+    .querySelector('#search-next-btn')
+    .addEventListener('click', findRoom);
+
+document
+    .querySelector('#new-reservation')
+    .addEventListener('click', cleanData);
+
+function fillSearchForm(event) {
+    event.preventDefault();
+
+    changeContent('search-form-content');
+
+    const checkInInput = document.querySelector('#check-in');
+    const checkOutInput = document.querySelector('#check-out');
+    const peopleInput = document.querySelector('#people');
+
+    if (checkInInput && checkOutInput && peopleInput) {
+        checkInInput.value = reservation.startDate;
+        checkOutInput.value = reservation.endDate;
+        peopleInput.value = reservation.guestsCount;
+    }
+}
+
+function selectRoomType(event) {
+    document
+        .querySelectorAll('.room-type')
+        .forEach(room => room.classList.remove('selected-room'));
+
+    event.currentTarget.classList.add('selected-room');
+}
+
+function findRoom(event) {
+    event.preventDefault();
+
+    const selectedRoom = document.querySelector('.selected-room h4');
+
+    if (!selectedRoom) {
+        return;
+    }
+
+    reservation.roomType = selectedRoom.textContent;
+
+    console.log(reservation);
+    changeContent('guest-details-form-content');
+}
+
+function cleanData(event) {
+    event.preventDefault();
     changeContent('search-form-content');
 }
